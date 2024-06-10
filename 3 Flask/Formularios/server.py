1a9f9a56610bd3221aaa3e5ac684ad85267ff982
jkammlec <jbkc_10@hotmail.com>
2024-06-10 11:21:31 -0400

    flask intro y form

--- 3 Flask/Formularios/server.py
+++ 3 Flask/Formularios/server.py
@@ -15,5 +15,26 @@ app = Flask(__name__) #Instancia de Flask = Aplicación web
 def index():
     return render_template("index.html")
 
+#2- Recibe la info del formulario
+@app.route("/registrar", methods=['post'])
+def registrar():
+    print(request.form) #imprime diccionario completo
+    print(request.form['email']) #imprime un valor del diccionario
+    return redirect("/exito")
+'''
+request.form = {
+    "nombre":"Juana de Arco",
+    "email":"email@email.com"
+    }
+'''
+#Renderizar = Mostrar una plantilla html
+#NUNCA renderizamos una plantilla cuando recibo un POST
+#En su lugar: redirigimos a otra ruta /exito
+
+#3- Ruta a la que redirigimos
+@app.route("/exito")
+def exito():
+    return render_template("exito.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
