ffea995321935f7617e33a1eecb2d3585b195b46
jkammlec <jbkc_10@hotmail.com>
2024-06-12 16:23:48 -0400

    actividad visitas

--- Actividades Core/Visitas/server.py
+++ Actividades Core/Visitas/server.py
@@ -2,28 +2,26 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
+app.secret_key = "llave del tesoro"
+
 @app.route("/")
 def index():
+    #la primera vez que entro
+    if "contador" not in session: #si no existe, contador es 0
+        session["contador"] = 0 #variable para mostrar visitas
+    else:
+        session["contador"] += 1 #ya existe, se suma 1
+
     return render_template("index.html")
 
-@app.route("/registrar", methods=['post'])
-def registrar():
-    #print(request.form)
-    #print(request.form['email'])
-    session["usuario"] = request.form["nombre"]
-    session["correo"] = request.form["email"]
-    return redirect("/exito")
-
-@app.route("/exito")
-def exito():
-    if "usuario" not in session:
-        return redirect("/")
-    return render_template("exito.html")
-
-@app.route("/logout")
-def logout():
-    #Cerrar sesión
+@app.route("/destruir_sesion")
+def destruir_sesion():
     session.clear()
+    return redirect("/")
+
+@app.route("/visitar_2")
+def visitar_2():
+    session["contador"] += 1
     return redirect("/")
 
 if __name__ == "__main__":
